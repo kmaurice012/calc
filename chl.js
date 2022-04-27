37e776a9714c46166798c5f5db1cfee9234e682e
kmaurice012 <kingmaurice012@gmail.com>
2022-04-27 08:59:10 +0300

    first commit

---
+++ chl.js
@@ -0,0 +1,9 @@
+// let hour = 600;
+// let minutes = 60;
+// let second = hour* minutes+ minutes;
+
+
+
+const input = prompt("Enter your first num");
+const input1 = prompt("Enter your second num");
+alert( "your answer is" + input * input1);
